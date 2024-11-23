37298667611d3b5faf136efb6a98bd376c1f8b54
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2024-11-23 13:51:22 -0500

    ...

--- src/app/lib/Redux/Slices/Playground/PlaygroundSlice.ts
+++ src/app/lib/Redux/Slices/Playground/PlaygroundSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AppThunk, RootState } from "../../Store";
+import { AppThunk, ReduxState } from "../../Store";
 import { fetchCount } from "./PlaygroundAPI";
 
 export interface PlaygroundState {
@@ -68,8 +68,8 @@ export const { increment, decrement, incrementByAmount } =
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.playground.value)`
-export const selectCount = (state: RootState) => state.playground.value;
+// in the slice file. For example: `useSelector((state: ReduxState) => state.playground.value)`
+export const selectCount = (state: ReduxState) => state.playground.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
