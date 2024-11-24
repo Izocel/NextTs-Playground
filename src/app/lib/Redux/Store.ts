f616b2841c047c3d9661bf93deae0c59fed11a6c
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2024-11-24 12:16:40 -0500

    fix export

--- src/app/lib/Redux/Store.ts
+++ src/app/lib/Redux/Store.ts
@@ -38,12 +38,12 @@ export const makeStore = () => {
 
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
+// Infer the `ReduxState` and `AppDispatch` types from the store itself
+export type ReduxState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
-  RootState,
+  ReduxState,
   unknown,
   Action<string>
 >;
